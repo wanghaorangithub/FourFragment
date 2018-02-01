853de335196f044382db0be20d1f05c0ad21336b
wanghaorangithub <wanghaorangithub@users.noreply.github.com>
2018-02-01 20:51:17 +0800

    格式化了OnButtonClick

--- app/src/main/java/com/example/a24078/fourfragment/OnButtonClick.java
+++ app/src/main/java/com/example/a24078/fourfragment/OnButtonClick.java
@@ -1,11 +1,11 @@
 package com.example.a24078.fourfragment;
 
 /**
- * Created by 24078 on 2018/1/29.
+ * Created by haoran on 2018/1/29.
  */
-
 
 public interface OnButtonClick {
     void switchTo1();
+
     void switchTo2();
 }
